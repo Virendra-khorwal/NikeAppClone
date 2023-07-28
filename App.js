d1393280b5cb79a4df4afb3150cc3f41cf7f5aed
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-07-28 19:25:17 +0530

    Home screen add

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from './data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image
+              source={{
+                uri: item.image,
+              }}
+              style={styles.image}
+            />
+          </View>
+        )}
+        numColumns={2}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +32,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer : {
+    width: '50%',
+    padding: 1,
+  }
 });
